cc96c4d2f29187d43819978b4c12c81cdc2030d0
Davisetims <etimsintegration@gmail.com>
2025-04-07 13:43:32 +0300

    `Added custom user model and admin interface with custom fields and forms`

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django import forms
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+
+User = get_user_model()  # Get the custom User model
+
+# Custom User Creation Form
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput,
+        min_length=8,  # Enforce minimum length
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput,
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "plan", "user_type")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords do not match.")
+        
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])  # Hash the password
+        if commit:
+            user.save()
+        return user
+
+# Custom UserAdmin
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "first_name", "last_name",  "plan", "user_type",
+                       "password1", "password2")
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin) 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    PLAN_CHOICES = (
+        ('basic', 'Basic'),
+        ('premium', 'Premium'),
+    )
+
+    USER_TYPES_CHOICES = (
+        ('contractor', 'Contractor'),
+        ('client', 'Client'),
+    )
+
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=12, choices=USER_TYPES_CHOICES, default='client')
+    plan = models.CharField(max_length=10, choices=PLAN_CHOICES, default='basic')
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f' {self.id}  - {self.username}'
