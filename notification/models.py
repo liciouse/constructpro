8ac3110b9ce6cfd90977fcfa154efae964d78fdb
Davisetims <etimsintegration@gmail.com>
2025-04-10 11:31:42 +0300

    `Added notification app and registered Message model in admin interface.`

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    subject = models.CharField(max_length=200)
+    body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.sender.username} -> {self.recipient.username}'
+
