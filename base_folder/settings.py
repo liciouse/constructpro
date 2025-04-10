acf0df8e54a94cc482a87d028c821b739a8efce0
Davisetims <etimsintegration@gmail.com>
2025-04-10 11:26:11 +0300

    `Added payment app and model to the project`

--- base_folder/settings.py
+++ base_folder/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users',
     'project',
     'material',
+    'payment',
 ]
 
 MIDDLEWARE = [
--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db import models# Create your models here.
+from project.models import Project
+from users.models import User
 
-# Create your models here.
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD = (
+        ('mpesa', 'M-pesa'),
+        ('bank', 'Bank'),
+        ('cash', 'Cash'),
+    )
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    payer = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        limit_choices_to= {'user_type': 'client'}, 
+        null=True, blank=True,
+        related_name='payments_made')
+    payee = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        limit_choices_to= {'user_type': 'contractor'},
+        null=True, blank=True,
+        related_name='payments_received')
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    payment_date = models.DateField()
+    method = models.CharField(max_length=7, choices=PAYMENT_METHOD, default='mpesa')  # e.g., M-Pesa, Bank, Cash
+
+    def __str__(self):
+        return f"{self.amount} from {self.payer} to {self.payee}"
+
